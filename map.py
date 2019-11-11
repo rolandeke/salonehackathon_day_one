020640e9fba54f55aff0236a74ee00ae598818f8
rolandeke <rolandeke49@gmail.com>
2019-11-11 06:44:48 +0000

    Delete All codes in map.py to start with a clean slate from scratch and also added a function to call to the api and return data back

--- map.py
+++ map.py
@@ -1,12 +1,45 @@
-import  requests 
+import requests
+from requests.exceptions import HTTPError
+import typing 
 import time
-currentTime = int(time.time()) #currentTime in second
-startTime   =  currentTime - 3600 * 48 # 48h in the past  
+from typing import Dict
+
 username = "pdtpatrick"
 password = "u3!WL2uC0dxu"
-endTime = int(time.time())
+current_time = int(time.time())
+start_time = current_time - (3600 * 48)
+end_time = current_time
 
-URL = f"https://opensky-network.org/api/flights/departure?airport=KSEA&begin={startTime}&end={endTime}"
-response = requests.get(URL, auth=(username, password)) 
-data = response.json()
-print(data[1])
+def get_data_from_api(airport:str,start_time:int,end_time:int):
+    """
+        Function calls to the API for all arrival flights at a particular Airport 
+        @params airport
+        @params start_time
+        @params end_time
+        
+        returns response_from_api
+    """
+    #connect to the api
+    URL = f"https://opensky-network.org/api/flights/arrival?airport={airport}&begin={start_time}&end={end_time}"
+    try:
+        response_from_api = requests.get(URL,auth=(username,password))
+        response_from_api.raise_for_status()
+    except HTTPError as error:
+        print(error)
+    else:
+        if response_from_api.status_code == 200:
+            assert len(response_from_api.json()) != 0
+            return response_from_api.json()
+        elif response_from_api.status_code == 404:
+            print("Data Not Found")
+            return None
+        
+
+            
+
+flights = get_data_from_api("KSEA",start_time,end_time)
+
+print(len(flights))
+
+    
+    
